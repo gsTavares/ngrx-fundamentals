6ab5ad0773ac2e0cbccf5a0690062576b02c86d1
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-06-10 08:56:54 -0300

    feat: initial component structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', component: HomeComponent,
+  },
+  {
+    path: 'book-list', component: BookListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
